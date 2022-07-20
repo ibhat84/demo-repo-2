3690f91c3b881ac72eaeffb947eec196be6dd95e
ibhat <bhats.texas@gmail.com>
2022-07-19 19:37:36 -0500

    edit comment

--- git-service/src/main/java/com/bhats/gitservice/GitServiceApplication.java
+++ git-service/src/main/java/com/bhats/gitservice/GitServiceApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GitServiceApplication {
 
+	
+	//Comment - to check
 	public static void main(String[] args) {
 		SpringApplication.run(GitServiceApplication.class, args);
 	}
